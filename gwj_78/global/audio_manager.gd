a0576b5b768a6162a12162ac244a585d1e1d8f9b
BaconEggsRL <apatton090@gmail.com>
2025-02-14 14:11:12 -0500

    add globals in case we need them

---
+++ gwj_78/global/audio_manager.gd
@@ -0,0 +1,217 @@
+extends Node
+
+@export var MUSIC_PLAYER:AudioStreamPlayer
+var ambient_players := {}
+var active_fx_players: Dictionary = {}  # Store FX players with names
+
+# set up audio bus names as follows:
+# Master, music, ambient, sfx
+
+
+# music
+# list of all music
+
+# ambient
+# list of all ambient sounds
+
+# fx
+# list of all sound effects
+
+
+
+
+
+func play_ambient(ambient_name: String, fade_in_time: float = 2.0, final_db: float = -6.0) -> void:
+	if ambient_name in ambient_players:
+		# push_warning("Ambient sound '%s' is already playing" % ambient_name)
+		return
+
+	var ambient_stream: AudioStream
+	match ambient_name:
+		"ambient_1":
+			ambient_stream = null  # insert ambient const here
+		_:
+			push_warning("'%s' has no resource listed in AudioManager" % ambient_name)
+			return
+
+	if ambient_stream:
+		
+		var ambient_player := AudioStreamPlayer.new()
+		ambient_player.stream = ambient_stream
+		ambient_player.bus = "ambient"  # Make sure you have an "Ambient" bus in the audio settings
+		ambient_player.volume_db = -80.0  # Start silent
+		# ambient_player.loop = true  # Ensure looping
+		add_child(ambient_player)
+		ambient_player.play()
+
+		# Store the player reference
+		ambient_players[ambient_name] = ambient_player
+
+		# Fade-in effect
+		var tween = get_tree().create_tween()
+		tween.tween_property(ambient_player, "volume_db", final_db, fade_in_time)
+
+
+func stop_ambient(ambient_name: String, fade_out_time: float = 2.0) -> void:
+	if ambient_name not in ambient_players:
+		push_warning("Ambient sound '%s' is not currently playing" % ambient_name)
+		return
+
+	var ambient_player = ambient_players[ambient_name]
+	var tween = get_tree().create_tween()
+	tween.tween_property(ambient_player, "volume_db", -80.0, fade_out_time)
+	await tween.finished
+
+	# Remove player from dictionary and free it
+	ambient_players.erase(ambient_name)
+	ambient_player.queue_free()
+	
+	
+func stop_music(fade_out_time=0.5, music_player:=MUSIC_PLAYER) -> void:
+	var tween_out = get_tree().create_tween()
+	tween_out.tween_property(music_player, "volume_db", -80.0, fade_out_time)
+	await tween_out.finished
+	# Ensure music stops and resets
+	music_player.stop()
+	music_player.stream = null  # Reset to ensure _play_music() reloads it
+		
+		
+func _play_music(
+	music:AudioStream, 
+	final_db:=0.0, 
+	fade_out:bool=true, 
+	fade_out_time:float=0.5,
+	fade_in:bool=true,
+	fade_in_time:float=0.5,
+	init:bool=false,
+	init_db:float = -80.0,
+	music_player:=MUSIC_PLAYER) -> void:
+
+	# If the music is currently being played already, do nothing
+	if music_player.stream == music:
+		return
+	
+	
+	# don't fade out prev track if there's no stream
+	if !music_player.stream:
+		fade_out = false
+
+	
+	# check if should fade audio in/out
+	if fade_out:
+		var tween_out = get_tree().create_tween()
+		tween_out.tween_property(music_player, "volume_db", -80.0, fade_out_time)
+		await tween_out.finished
+	
+	else:
+		
+		# check if we need to initialize the volume
+		if init:
+			music_player.volume_db = init_db
+		
+		if fade_in:
+			# audio_player.stop()
+			music_player.stream = music
+			music_player.play()
+			
+			var tween_in = get_tree().create_tween()
+			tween_in.set_ease(Tween.EASE_OUT)
+			tween_in.set_trans(Tween.TRANS_CUBIC)
+			tween_in.tween_property(music_player, "volume_db", final_db, fade_in_time)
+			await tween_in.finished
+		
+		else:
+			
+			# audio_player.stop()
+			music_player.stream = music
+			music_player.volume_db = final_db
+			music_player.bus = "music"
+			music_player.play()
+
+
+# Play music for scene
+func play_music(scene_name:String, final_db:float=0.0, fade_in_time=0.5) -> void:
+	var music:Resource
+	var fade_out:bool = true
+	var fade_out_time = 0.5
+	var fade_in:bool = true
+	var init:bool = false
+	var init_db:float = -80.0
+	match scene_name:
+		"music_1":
+			music = null  # insert music const here
+		_:
+			push_warning("'%s' has no resource listed in AudioManager" % scene_name)
+	
+	if music:
+		_play_music(music, final_db, fade_out, fade_out_time, fade_in, fade_in_time, init, init_db)
+
+
+func play_fx(fx_name:String, volume:float=0.0, _index:int=-1) -> void:
+	var fx:Resource
+	var pitch:float = 1.0
+	match fx_name:
+		"pitch_var_1":
+			fx = null  # insert fx const here
+			var diff = 0.2
+			pitch = randf_range(1.0 - diff, 1.0 + diff)
+		"random_1":
+			fx = [].pick_random()  # can pick random from array of sounds
+		_:
+			push_warning("'%s' has no resource listed in AudioManager" % fx_name)
+	
+	if fx:
+		_play_fx(fx_name, fx, volume, pitch)
+	
+	
+	
+	
+func _play_fx(fx_name: String, stream: AudioStream, volume: float = 0.0, pitch: float = 1.0) -> void:
+	var fx_player = AudioStreamPlayer.new()
+	fx_player.stream = stream
+	fx_player.name = "FX_PLAYER_" + fx_name  # Name it uniquely
+	fx_player.volume_db = volume
+	fx_player.pitch_scale = pitch
+	fx_player.bus = "sfx"
+	
+	add_child(fx_player)
+	
+	if not active_fx_players.has(fx_name):
+		active_fx_players[fx_name] = []
+	
+	active_fx_players[fx_name].append(fx_player)  # Track this FX under its name
+	
+	if fx_player != null:
+		fx_player.play()
+		await fx_player.finished
+		
+		if active_fx_players.has(fx_name):
+			active_fx_players[fx_name].erase(fx_player)  # Remove from tracking
+			fx_player.queue_free()
+
+
+func stop_fx(fx_name: String, fade_time: float = 0.5) -> void:
+	if active_fx_players.has(fx_name):
+		for fx_player in active_fx_players[fx_name]:
+			if is_instance_valid(fx_player):  # Ensure it's still valid
+				var tween = get_tree().create_tween()
+				tween.tween_property(fx_player, "volume_db", -80.0, fade_time)  # Fade out
+				await tween.finished
+				if fx_player != null:
+					fx_player.stop()
+					fx_player.queue_free()
+		
+		active_fx_players.erase(fx_name)  # Remove all instances of that FX
+
+
+func clear(fade_time:float = 2.0) -> void:
+	# stop music
+	stop_music(fade_time)
+	# clear fx
+	if active_fx_players:
+		var fx_players = active_fx_players.keys()
+		for fx_player in fx_players:
+			if active_fx_players.has(fx_player):  # Ensure key still exists
+				stop_fx(fx_player, fade_time)
+		
+		
---
+++ gwj_78/global/scene_manager.gd
@@ -0,0 +1,82 @@
+extends Node
+
+
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+
+
+# scenes
+const MAIN = preload("res://scenes/main.tscn")
+const GAME = preload("res://scenes/game.tscn")
+
+var next_scene:Resource
+
+
+
+
+func _ready() -> void:
+	# save_data = SaveData.load_or_create()
+	# set bus volume
+	# set_bus_volume()
+	pass
+	
+	
+# set volume of each bus from save data
+#func set_bus_volume() -> void:
+	#for bus_name in save_data.bus_volume:
+		#var bus_index = AudioServer.get_bus_index(bus_name)
+		#var value = save_data.bus_volume[bus_name]
+		#AudioServer.set_bus_volume_db(
+			#bus_index,
+			#linear_to_db(value)
+		#)
+		
+		
+		
+		
+		
+
+func _play_animation(anim_name: String) -> void:
+	if !animation_player.has_animation(anim_name):
+		push_warning("'%s' animation does not exist in SceneManager" % anim_name)
+	else:
+		animation_player.play(anim_name)
+
+
+func _load_next_scene(scene:Resource, custom_data:Dictionary={}) -> void:
+	var tree: SceneTree = get_tree()
+	tree.change_scene_to_packed(scene)
+	tree.root.child_entered_tree.connect(_on_child_entered_tree.bind(custom_data), CONNECT_ONE_SHOT)
+
+
+func _on_child_entered_tree(scene:Node, custom_data:Dictionary={}) -> void:
+	assert(scene == get_tree().current_scene) # at this point it's set
+	if custom_data != {}:
+		for property in scene.get_property_list():
+			if property.name == "custom_data":
+				print("custom data!")
+				scene.custom_data = custom_data # pass custom data or whatever
+				return
+		print("no custom data!")
+	
+
+func _goto(scene:Resource, custom_data:Dictionary={}, fade_out:String="fade_out", fade_in:String="fade_in") -> void:
+	AudioManager.play_fx("scene_transition")
+	_play_animation(fade_out)
+	await animation_player.animation_finished
+	_load_next_scene(scene, custom_data)
+	_play_animation(fade_in)
+	await animation_player.animation_finished
+	
+	
+func goto(scene_name:String, custom_data:Dictionary={}, fade_out:String="fade_out", fade_in:String="fade_in") -> void:
+	var scene:Resource
+	match scene_name:
+		#"main":
+			#scene = MAIN
+		"game":
+			scene = GAME
+		_:
+			push_warning("'%s' has no resource listed in SceneManager" % scene_name)
+	
+	if scene:
+		_goto(scene, custom_data, fade_out, fade_in)
