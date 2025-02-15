5ab6deb874cf6755844e74a4240a4079ec5d3571
BaconEggsRL <apatton090@gmail.com>
2025-02-15 11:39:17 -0500

    asd

--- gwj_78/scenes/mouse.gd
+++ gwj_78/scenes/mouse.gd
@@ -1,12 +1,36 @@
 extends Sprite2D
 
 
-const ERASER = preload("res://assets/art/mouse/eraser.png")
-const PENCIL = preload("res://assets/art/mouse/pencil.png")
+const eraser_sprite = preload("res://assets/art/mouse/eraser.png")
+const pencil_sprite = preload("res://assets/art/mouse/pencil.png")
 
 
-func _ready() -> void:
-	self.texture = ERASER
+enum State {
+	ERASER, PENCIL
+}
+var current_state = State.ERASER
+
+
+func set_state(new_state:State) -> void:
+	self.current_state = new_state
+	match current_state:
+		State.ERASER:
+			self.texture = eraser_sprite
+		State.PENCIL:
+			self.texture = pencil_sprite
+			
 	
+	
+func _ready() -> void:
+	set_state(State.ERASER)
+	
+
+
+func _input(event):
+	if event is InputEventMouseButton and event.pressed:
+		var new_state = (current_state + 1) % State.size()
+		set_state(new_state)
+		
+		
 func _process(_delta) -> void:
 	self.global_position = get_global_mouse_position()
