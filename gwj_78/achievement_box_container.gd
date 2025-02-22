bd8be7f1687408c2db2f6f9cb00778e6803c33e0
BaconEggsRL <apatton090@gmail.com>
2025-02-22 09:51:12 -0500

    asd

---
+++ gwj_78/achievement_box_container.gd
@@ -0,0 +1,7 @@
+class_name AchievementBoxContainer
+extends Control
+
+@onready var shadow_btn: Button = $shadow_btn
+@onready var icon_rect: TextureRect = $shadow_btn/icon_rect
+@onready var frame_btn: Button = $shadow_btn/icon_rect/frame_btn
+@onready var name_label: Label = $name_label
--- gwj_78/scenes/achievements.gd
+++ gwj_78/scenes/achievements.gd
@@ -8,6 +8,9 @@ extends Node2D
 # save data
 var save_data:SaveData
 
+# box
+const ACHIEVEMENT_BOX_CONTAINER = preload("res://achievement_box_container.tscn")
+
 
 
 func _ready() -> void:
@@ -32,22 +35,33 @@ func _ready() -> void:
 func display_achievements() -> void:
 	var achievements = save_data.get_all_achievements()
 	for ach in achievements:
-		var hbox = HBoxContainer.new()
+		add_achievement_box(ach.icon, ach.ach_name.replace("_", " ").capitalize(), ach.hover_text)
 		
-		var icon = TextureRect.new()
-		icon.texture = ach.icon
-		icon.stretch_mode = TextureRect.STRETCH_KEEP_ASPECT_CENTERED
-		icon.custom_minimum_size = Vector2(64, 64)
-		icon.tooltip_text = ach.hover_text
-		
-		var label = Label.new()
-		label.text = ach.ach_name.replace("_", " ").capitalize()
-		label.tooltip_text = ach.hover_text
-		
-		hbox.add_child(icon)
-		hbox.add_child(label)
-		grid_container.add_child(hbox)
-		
-			
+
+
+func add_achievement_box(icon:Texture2D, label_text:String, hover_text:String) -> void:
+	var box:AchievementBoxContainer = ACHIEVEMENT_BOX_CONTAINER.instantiate()
+	grid_container.add_child(box)
+	box.icon_rect.texture = icon
+	box.name_label.text = label_text
+	box.tooltip_text = hover_text
+	
+	#var box = VBoxContainer.new()
+	#
+	#var icon = TextureRect.new()
+	#icon.texture = ach.icon
+	#icon.stretch_mode = TextureRect.STRETCH_KEEP_ASPECT_CENTERED
+	#icon.custom_minimum_size = Vector2(64, 64)
+	#icon.tooltip_text = ach.hover_text
+	#
+	#var label = Label.new()
+	#label.text = ach.ach_name.replace("_", " ").capitalize()
+	#label.tooltip_text = ach.hover_text
+	#label.horizontal_alignment = HORIZONTAL_ALIGNMENT_CENTER
+	#
+	#box.add_child(icon)
+	#box.add_child(label)
+
+
 func _to_main() -> void:
 	SceneManager.goto("main")
